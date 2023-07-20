643597841a8dcf368b7f722d581107d65c4fdb9f
charlesDinneya <dinneyacharles007@gmail.com>
2023-07-20 22:30:13 +0200

    issue fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"github.com/codingwithik/simple-blog-backend-with-go/cmd/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 
 	"github.com/codingwithik/simple-blog-backend-with-go/src/config"
@@ -8,13 +12,22 @@ import (
 
 func main() {
 	// Initialize Database
-	config, err := config.LoadConfig(".")
+	conf, err := config.LoadConfig(".")
 	if err != nil {
 		log.Fatal("? Could not load environment variables", err)
 	}
-	config.ConnectDB(&config)
-	config.Migrate()
+	config.ConnectDB(&conf)
+	err = config.Migrate()
+	if err != nil {
+		log.Fatal("? Could not migrate:", err)
+	}
+
 	// Initialize Router
-	//router := initRouter()
-	//router.Run(":8080")
+
+	docs.SwaggerInfo.BasePath = "/api/v1"
+
+	router := gin.Default()
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	log.Fatal(router.Run(":8080"))
+
 }
